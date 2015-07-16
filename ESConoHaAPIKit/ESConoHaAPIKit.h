364cf9c54f6e56f86991cfe60a8c57eb3baefa82
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-16 20:46:48 +0900

    Initial Commit

---
+++ ESConoHaAPIKit/ESConoHaAPIKit.h
@@ -0,0 +1,19 @@
+//
+//  ESConoHaAPIKit.h
+//  ESConoHaAPIKit
+//
+//  Created by Tomohiro Kumagai on H27/07/16.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESConoHaAPIKit.
+FOUNDATION_EXPORT double ESConoHaAPIKitVersionNumber;
+
+//! Project version string for ESConoHaAPIKit.
+FOUNDATION_EXPORT const unsigned char ESConoHaAPIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESConoHaAPIKit/PublicHeader.h>
+
+
